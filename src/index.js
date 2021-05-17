6188bd2d82afd8c88370e5862202f915412d844c
orlovse <hajoff@gmail.com>
2021-05-17 17:20:50 +0300

    add test routs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import "../static/styles.scss";
+
+const path = window.location.pathname;
+const appRouts = {
+  "/home": 1,
+  "/": 1,
+  "/login": 2,
+  "/signin": 3,
+  "/profile": 4,
+  "/chat": 5,
+};
+
+if (appRouts[path]) {
+  console.log(appRouts[path]);
+}
