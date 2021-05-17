3f7b378c53c74c72c82ae0d5f503361aba9fc322
orlovse <hajoff@gmail.com>
2021-05-17 21:58:49 +0300

    add first style of chat page with mock data

--- src/pages/chat/chat.js
+++ src/pages/chat/chat.js
@@ -1,6 +1,21 @@
 import { createTemplate } from "../../template";
+import "./chat.scss";
 
 const chatTemplate = `
+<div class="chat">
+  <div class="contacts-list">
+    <input class="search" placeholder="search"></input>
+    <div class="contact">
+      <div class="avatar"></div>
+      <div class="content">
+        <p class="title">Test contact</p>
+        <p class="subtitle">Message text</p>
+      </div>
+      <div class="badge"><span>2</span></div>
+    </div>
+  </div>
+  <div class="main-field"></div>
+</div>
 `;
 
 const chatData = {};
