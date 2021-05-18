5a9f574375b51fa7048768f92662cd7f674abe7a
orlovse <hajoff@gmail.com>
2021-05-18 21:21:28 +0300

    add avatar to message

--- src/components/message/message.js
+++ src/components/message/message.js
@@ -1,13 +1,18 @@
 import { createTemplate } from "../../template";
 import "./message.scss";
+import Avatar from "../avatar";
 
 const Message = (props) => {
   const messageTemplate = `
-    <div class="message {{props.class}}"> 
-    {{ props.text }} 
-  </div>`;
+    <div class="message {{ props.class }}"> 
+      <div class="message-avatar">
+        {{Avatar}}
+      </div>
+      {{ props.text }} 
+    </div>`;
   const messageData = {
     props,
+    Avatar: Avatar,
   };
   return createTemplate(messageTemplate, messageData);
 };
