68ca8a1a5a425f00ec25b981053cc05ffa9612ea
orlovse <hajoff@gmail.com>
2021-05-19 21:40:58 +0300

    add registration component

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,22 +1,33 @@
 import { createTemplate } from "../../template";
 import LoginComponent from "../../components/login";
+import RegistrationComponent from "../../components/registration";
 import "./login.scss";
 
 const Login = (props) => {
   const loginTemplate = `
     <div class="login">
-      <div class="window">
-        <div class="tab">
-          <a>Login</a>
-          <a>Registration</a>
+      <div class="sheet">
+        <div class="tabs">
+          <input type="radio" name="tabs" id="tab1" aria-controls="login" checked>
+          <label for="tab1">Login</label>
+          <input type="radio" name="tabs" id="tab2" aria-controls="registration">
+          <label for="tab2">Registration</label>
+          <div class="tab-panels">
+            <section class="tab-panel" id="login">
+              {{LoginComponent}}
+            </section>
+            <section class="tab-panel" id="registration">
+              {{RegistrationComponent}}
+            </section>
+          </div>
         </div>
-        {{LoginComponent}}
       </div>
     </div>
   `;
 
   const loginData = {
     LoginComponent,
+    RegistrationComponent,
   };
 
   return createTemplate(loginTemplate, loginData);
