2dffb745d4a7112fe089756906de060128ce6f85
orlovse <hajoff@gmail.com>
2021-05-29 19:56:40 +0300

    fixed css

--- src/components/badge/badge.js
+++ src/components/badge/badge.js
@@ -2,7 +2,7 @@ import "./badge.scss";
 import { createTemplate } from "../../template";
 
 const Badge = (props) => {
-  const badgeTemplate = `<div class="badge"><span>{{ props.number }}</span></div>`;
+  const badgeTemplate = `<div class="badge">{{ props.number }}</div>`;
   const badgeData = { props };
 
   return createTemplate(badgeTemplate, badgeData);
