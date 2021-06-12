b86f285dd434f3063ba37f0590b88420211702c7
orlovse <hajoff@gmail.com>
2021-06-12 20:42:30 +0300

    add events to profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -7,13 +7,15 @@ const Profile = () => {
   const profileTemplate = `
   <div class="profile">
     <div class="sheet">
-      <div class="avatar"></div>
-      <div class="fields">
-        {{ components.fieldsConfig }}
-      </div>
-      <div class="actions">
-        {{ components.Button }}
-      </div>
+      <form class="profile-form">    
+        <div class="avatar"></div>
+        <div class="fields">
+          {{ components.fieldsConfig }}
+        </div>
+        <div class="actions">
+          {{ components.SubmitButton }}
+        </div>
+      </form>
     </div>
   </div>
   `;
@@ -21,18 +23,61 @@ const Profile = () => {
   const profileData = {
     components: {
       fieldsConfig: [
-        Input({ label: "email", value: "mail@test.com", class: "mb-1" }),
-        Input({ label: "login", value: "orlov", class: "mb-1" }),
-        Input({ label: "name", value: "Sergey", class: "mb-1" }),
-        Input({ label: "surname", value: "Orlov", class: "mb-1" }),
-        Input({ label: "nickname", value: "Sergey", class: "mb-1" }),
-        Input({ label: "phone", value: "+79140000000", class: "mb-1" }),
+        Input({ label: "email", class: "profile-email mb-1" }),
+        Input({ label: "login", class: "profile-login mb-1" }),
+        Input({ label: "name", class: "profile-name mb-1" }),
+        Input({ label: "surname", class: "profile-surname mb-1" }),
+        Input({ label: "nickname", class: "profile-nickname mb-1" }),
+        Input({ label: "phone", class: "profile-phone mb-1" }),
       ],
-      Button: Button({ name: "Save" }),
+      SubmitButton: Input({
+        label: "Save",
+        type: "submit",
+        value: "Save",
+        class: "save-profile-button mt-2",
+      }),
+    },
+    result: {
+      email: "",
+      login: "",
+      name: "",
+      surname: "",
+      nickname: "",
+      phone: "",
     },
   };
 
-  return createElement(profileTemplate, profileData);
+  const profileEvents = [
+    {
+      selector: ".profile-form",
+      event: "submit",
+      func(e: Event) {
+        e.preventDefault();
+        console.log("test", this.get("result"));
+      },
+    },
+    {
+      selector: ".profile-form",
+      event: "input",
+      func(e: { target: HTMLInputElement }) {
+        if (e.target.classList.contains("profile-email")) {
+          this.set("result.email", e.target.value, true);
+        } else if (e.target.classList.contains("profile-login")) {
+          this.set("result.login", e.target.value, true);
+        } else if (e.target.classList.contains("profile-name")) {
+          this.set("result.name", e.target.value, true);
+        } else if (e.target.classList.contains("profile-surname")) {
+          this.set("result.surname", e.target.value, true);
+        } else if (e.target.classList.contains("profile-nickname")) {
+          this.set("result.nickname", e.target.value, true);
+        } else if (e.target.classList.contains("profile-phone")) {
+          this.set("result.phone", e.target.value, true);
+        }
+      },
+    },
+  ];
+
+  return createElement(profileTemplate, profileData, profileEvents);
 };
 
 export default Profile;
