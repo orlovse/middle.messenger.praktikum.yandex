cce0b99f485018ee3f12e0d945df1bdab377d5e1
orlovse <hajoff@gmail.com>
2021-06-12 13:30:21 +0300

    fix events

--- src/template/index.js
+++ src/template/index.js
@@ -1,4 +1,4 @@
-import { getObjectField, createId } from "../utils";
+import { getObjectField, createId, deepUpdate } from "../utils";
 
 const TEMPLATE_REGEXP = /\{\{(.*?)\}\}/i;
 
@@ -37,18 +37,26 @@ export const createElement = (template, data, events) => {
       .firstChild;
     element.setAttribute("data-id", dataId);
 
-    if (events && events.length > 0) {
-      events.map(({ selector, event, func }) => {
-        element
-          .querySelector(selector)
-          .addEventListener(event, func.bind(rData));
-      });
-    }
     if (childComponents.length > 0) {
       childComponents.map(({ dataId, data }) => {
         const portal = element.querySelector(`[data-id="${dataId}"]`);
         if (portal) {
           portal.replaceWith(data);
+        }
+      });
+    }
+    if (events && events.length > 0) {
+      events.map(({ selector, event, func }) => {
+        let targetEl = null;
+        if (selector === "root") {
+          targetEl = element;
+        } else {
+          targetEl = element.querySelector(selector);
+        }
+
+        //console.log("element, selector, targetEl", element, selector, targetEl);
+        if (targetEl) {
+          targetEl.addEventListener(event, func.bind(rData));
         }
       });
     }
@@ -68,12 +76,18 @@ export const createElement = (template, data, events) => {
 };
 
 const reactivData = (data) => {
-  const set = (key, value) => {
-    console.log("set", value);
-    console.log("getAll", data);
-    console.log("this", this);
-    data[key] = value;
-    data.updateElement(data.id);
+  const set = (key, value, notUpdate = false) => {
+    // console.log("set", value);
+    // console.log("getAll", data);
+    // console.log("this", this);
+    if (key.includes(".")) {
+      data = deepUpdate(data, key, value);
+    } else {
+      data[key] = value;
+    }
+    if (!notUpdate) {
+      data.updateElement(data.id);
+    }
   };
   const addUpdate = (update) => {
     data.updateElement = update;
