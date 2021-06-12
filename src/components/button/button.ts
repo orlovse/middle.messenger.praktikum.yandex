5f26a13584dd5da34ad1138a4d3ec9ab9791dee4
orlovse <hajoff@gmail.com>
2021-06-12 20:40:49 +0300

    add class prop to Button component

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -2,11 +2,12 @@ import { createElement } from "../../template";
 import "./button.scss";
 
 type Props = {
-  name: string;
+  name?: string;
+  class?: string;
 };
 
 const Button = (props: Props) => {
-  const buttonTemplate = `<button class="button">{{ props.name }}</button>`;
+  const buttonTemplate = `<button class="button {{ props.class }}">{{ props.name }}</button>`;
   const buttonData = { props };
 
   return createElement(buttonTemplate, buttonData);
