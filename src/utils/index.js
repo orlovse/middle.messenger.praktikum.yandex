9d601e8bd3bcc623006f0d4510442901693ecafe
orlovse <hajoff@gmail.com>
2021-06-12 13:30:02 +0300

    add deepUpdate function to util

--- src/utils/index.js
+++ src/utils/index.js
@@ -15,3 +15,31 @@ export const getObjectField = (obj, path, defaultValue) => {
 export const createId = () => {
   return Date.now().toString(36) + Math.random().toString(36).substr(2);
 };
+
+export const deepUpdate = (original, keys, value) => {
+  if (typeof keys === "string") {
+    keys = keys.split(".");
+  }
+  if (keys.length === 0) {
+    return value;
+  }
+  const currentKey = keys[0];
+  if (Array.isArray(original)) {
+    return original.map((v, index) =>
+      index === currentKey ? deepUpdate(v, keys.slice(1), value) : v
+    );
+  } else if (typeof original === "object" && original !== null) {
+    return Object.fromEntries(
+      Object.entries(original).map((keyValuePair) => {
+        const [k, v] = keyValuePair;
+        if (k === currentKey) {
+          return [k, deepUpdate(v, keys.slice(1), value)];
+        } else {
+          return keyValuePair;
+        }
+      })
+    );
+  } else {
+    return original;
+  }
+};
