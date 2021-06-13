38cbe09300a7ea5c373a7cfe126ab23040edf8b2
orlovse <hajoff@gmail.com>
2021-06-13 21:39:16 +0300

    for test

--- api/server.js
+++ api/server.js
@@ -3,6 +3,9 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
+//just check test
+const http = require("http");
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
