2fd7d8e5dafdfa5f4f4dcc1d3cf84442b51d012b
orlovse <hajoff@gmail.com>
2021-06-13 21:13:09 +0300

    add boilerplate component

--- src/components/_boilerplate/component.ts
+++ src/components/_boilerplate/component.ts
@@ -1,4 +1,4 @@
-import { createElement } from "../../template/index";
+import { createElement, reactivData } from "../../template/index";
 import "./component.scss";
 import Button from "../button";
 
@@ -7,38 +7,39 @@ type Props = {
 };
 
 const Component = (props: Props) => {
-  // const template = `
-  //   <div class="{{ props.class }}">
-  //     <div class="name">{{ name }}</div>
-  //     {{ components.componentsList }}
-  //     <div>{{ components.Button }}</div>
-  //   </div>
-  // `;
-  // const data = {
-  //   props,
-  //   components: {
-  //     componentsList: [],
-  //     Button: Button({}),
-  //   },
-  //   name: "Test component",
-  //   someData: {
-  //     firstValue: "test",
-  //   },
-  // };
-  // const events = [
-  //   {
-  //     selector: "root",
-  //     event: "click",
-  //     func(e: Event) {
-  //       console.log("func must not be arrow.");
-  //       //get data, change data:
-  //       const newValue = "new value";
-  //       this.set("someData.firstValue", newValue);
-  //       console.log(this.get("someData.firstValue"));
-  //     },
-  //   },
-  // ];
-  // return createElement(template, data, events);
+  const template = `
+    <div class="{{ props.class }}">
+      <div class="name">{{ name }}</div>
+      {{ components.componentsList }}
+      <div>{{ components.Button }}</div>
+    </div>
+  `;
+  const rData = reactivData({
+    props,
+    name: "Test component",
+    someData: {
+      firstValue: "test",
+    },
+  });
+
+  const components = {
+    componentsList: [Button({}), Button({}), Button({})],
+    Button: Button({}),
+  };
+  const events = [
+    {
+      selector: "root",
+      event: "click",
+      func(e: Event) {
+        console.log("func must not be arrow.");
+        //get data, change data:
+        const newValue = "new value";
+        rData.set("someData.firstValue", newValue);
+        console.log(rData.get("someData.firstValue"));
+      },
+    },
+  ];
+  return createElement({ template, rData, events, components });
 };
 
 export default Component;
