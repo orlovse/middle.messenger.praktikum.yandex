02b14d90adbf19a898cefbb2e91a73cb6d55638d
orlovse <hajoff@gmail.com>
2021-06-13 21:41:10 +0300

    delete

--- api/server.js
+++ api/server.js
@@ -3,9 +3,6 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
-//just check test
-const http = require("http");
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
