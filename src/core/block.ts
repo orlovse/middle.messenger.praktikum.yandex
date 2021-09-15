f2f4a0c8a933d564a7d4817a1346ec9ad81a2788
orlovse <hajoff@gmail.com>
2021-09-15 16:51:22 +0300

    change block class

--- src/core/block.ts
+++ src/core/block.ts
@@ -1,173 +1,146 @@
-import { EventBus } from "./eventBus";
-
-export function parseStringToHtml(blockAsString: string): HTMLElement {
-  const blockAsHtml = new DOMParser().parseFromString(blockAsString, "text/html").body.firstChild;
-  return blockAsHtml as HTMLElement || document.createElement("div");
-}
-
-export type TProps = {
-    [key: string]: string | number | boolean | Function | undefined | Object | Array<Object>, // eslint-disable-line @typescript-eslint/ban-types
-    onClick?: (e?: Event) => void,
-    onBlur?: (e?: Event) => void,
-};
-export type TChildren = {[key: string]: any}; // eslint-disable-line @typescript-eslint/no-explicit-any
-
-export interface IBlock {
-    element: HTMLElement;
-    init: () => void;
-    hide: () => void;
-    show: () => void;
-    render: () => void;
-    componentDidMount: () => void;
-    componentDidUpdate: () => boolean;
-    setProps: (nextProps: unknown) => void;
-    getElement: () => HTMLElement;
-}
+import { createHTMLfromTemplate } from '../utils/createHTMLfromTemplate';
+import { BlockPropsType, ChildrenType, IBlock } from '../types';
+import { EventBus } from './eventBus';
 
 export class Block implements IBlock {
-    static EVENTS = {
-      INIT: "init",
-      FLOW_CDM: "flow:component-did-mount",
-      FLOW_CDU: "flow:component-did-update",
-      FLOW_RENDER: "flow:render"
-    };
-  
-    _element: HTMLElement;
-    props: TProps;
-    children: TChildren;
-    eventBus: EventBus;
-  
-    constructor(props: TProps, children: TChildren)  {
-        const eventBus = new EventBus();
-  
-        this.props = this._makePropsProxy(props) || {};
-        this.children = children || {};
-    
-        this.eventBus = eventBus;
-    
-        this._registerEvents(eventBus);
-        eventBus.emit(Block.EVENTS.INIT);
+  static EVENTS = {
+    INIT: 'init',
+    FLOW_CDM: 'flow:component-did-mount',
+    FLOW_CDU: 'flow:component-did-update',
+    FLOW_RENDER: 'flow:render'
+  };
+
+  _element: HTMLElement;
+  children: ChildrenType;
+  eventBus: EventBus;
+  props: BlockPropsType;
+
+  constructor(props: BlockPropsType, children: ChildrenType) {
+    const eventBus = new EventBus();
+
+    this.props = this._makePropsProxy(props) || {};
+    this.children = children || {};
+    this.eventBus = eventBus;
+
+    this._registerEvents(eventBus);
+    eventBus.emit(Block.EVENTS.INIT);
+  }
+
+  _registerEvents(eventBus: EventBus) {
+    eventBus.on(Block.EVENTS.INIT, this.init.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_CDM, this._componentDidMount.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_CDU, this._componentDidUpdate.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_RENDER, this._render.bind(this));
+  }
+
+  _makePropsProxy(props: BlockPropsType) {
+    const proxyProps = new Proxy(props, {
+      set(target: BlockPropsType, prop: string, value) {
+        target[prop] = value;
+        return true;
+      },
+      deleteProperty() {
+        throw new Error('No access');
+      }
+    });
+    return proxyProps;
+  }
+
+  _addChildren(children: ChildrenType) {
+    for (const childName in children) {
+      const child = children[childName];
+      const childProps = child.props;
+      const elementChild = this._element.querySelector(
+        `[data-component="${childName}"]`
+      );
+      const component = child.element;
+      elementChild?.appendChild(component);
+
+      if (childProps.onClickChild) {
+        const onClick = childProps.onClickChild(this.setProps, this.props);
+        elementChild?.addEventListener('click', onClick);
+      }
     }
-  
-    _registerEvents(eventBus: EventBus): void {
-        eventBus.on(Block.EVENTS.INIT, this.init.bind(this));
-        eventBus.on(Block.EVENTS.FLOW_CDM, this._componentDidMount.bind(this));
-        eventBus.on(Block.EVENTS.FLOW_CDU, this._componentDidUpdate.bind(this));
-        eventBus.on(Block.EVENTS.FLOW_RENDER, this._render.bind(this));
+  }
+
+  _createResources() {
+    this._element = document.createElement('div');
+  }
+
+  init() {
+    this._createResources();
+    this.eventBus.emit(Block.EVENTS.FLOW_CDM);
+  }
+
+  _componentDidMount() {
+    this.componentDidMount();
+    this.eventBus.emit(Block.EVENTS.FLOW_RENDER);
+  }
+
+  _componentDidUpdate(oldProps: BlockPropsType, newProps: BlockPropsType) {
+    if (JSON.stringify(newProps) === JSON.stringify(oldProps)) {
+      return false;
     }
 
-    _makePropsProxy(props: TProps): TProps {
-        const proxyProps = new Proxy(props, {
-            // eslint-disable-next-line
-            // @ts-ignore
-            set(target: TProps, prop: keyof TProps, value) {
-                target[prop] = value;
-                return true;
-            },
-            deleteProperty() {
-                throw new Error("нет доступа");
-            }
-        });
-        return proxyProps;
+    const response = this.componentDidUpdate();
+    this.eventBus.emit(Block.EVENTS.FLOW_RENDER);
+
+    return response;
+  }
+
+  _render() {
+    const block = createHTMLfromTemplate(this.render());
+    this._element.firstElementChild
+      ? this._element.replaceChild(block, this._element.firstElementChild)
+      : this._element.append(block);
+
+    if (this.props.onClick) {
+      block.addEventListener('click', this.props.onClick);
     }
 
-    _addChildrenEventListeners(children: TChildren): void {
-        for (const name in children) {
-          const child = children[name]
-            const childProps = child.props;
-            const elementChild = this._element.querySelector(`[data-component="${name}"]`);
-            const component = child.element
-            //const componentToRender = component.firstChild || component
-            elementChild?.appendChild(component)
-
-            if(childProps.onClickChild) {
-                const ab = childProps.onClickChild(this.setProps, this.props)
-                elementChild?.addEventListener("click", ab);
-            }
-
-            // if (childProps.onClick) {
-            //     const elementChildren = this._element.querySelectorAll(`[data-component="${name}"]`);
-            //     for (const child of elementChildren) {
-            //       console.log('childProps', childProps)
-            //         child.addEventListener("click", childProps.onClick);
-            //     }
-            // }
-        }
-    }
-  
-    _createResources(): void {
-        const tagName = "div";
-        this._element = document.createElement(tagName);
-    }
-  
-    init(): void {
-        this._createResources();
-        this.eventBus.emit(Block.EVENTS.FLOW_CDM);
-    }
-  
-    _componentDidMount(): void {
-        this.componentDidMount();
-        this.eventBus.emit(Block.EVENTS.FLOW_RENDER);
-    }
-  
-    _componentDidUpdate(oldProps: TProps, newProps: TProps): boolean {
-
-        if (JSON.stringify(newProps) == JSON.stringify(oldProps)) {
-            return false;
-        }
-        const response = this.componentDidUpdate();
-        //this.eventBus.emit(Block.EVENTS.FLOW_RENDER);
-
-        return response;
-    }
-  
-    _render(): void {
-        const block = parseStringToHtml(this.render());
-        this._element.firstElementChild  ? this._element.replaceChild(block, this._element.firstElementChild ) : this._element.append(block);
-        
-        if (this.props.onClick) {
-            block.addEventListener("click", this.props.onClick);
-        }
-
-        if (this.props.onBlur) {
-          if(block.tagName === "input") {
-            block.addEventListener("blur", this.props.onBlur);
-          }else {
-            block.querySelector('input')?.addEventListener("blur", this.props.onBlur);
-          }
-
-        }
-        this._addChildrenEventListeners(this.children);
+    if (this.props.onBlur) {
+      if (block.tagName === 'input') {
+        block.addEventListener('blur', this.props.onBlur);
+      } else {
+        block
+          .querySelector('input')
+          ?.addEventListener('blur', this.props.onBlur);
+      }
     }
 
-    setProps = (nextProps: TProps): void => {
-        if (!nextProps) {
-            return;
-        }
-        console.log('nextProps', nextProps, this.props)
-        const oldProps = {...this.props};
-        this.props = {...oldProps, ...nextProps};
-        //console.log('this.props', oldProps, this.props)
-        this.eventBus.emit(Block.EVENTS.FLOW_CDU, oldProps, nextProps);
-    };
+    this._addChildren(this.children);
+  }
 
-    componentDidMount(): void {} // eslint-disable-line @typescript-eslint/no-empty-function
-    componentDidUpdate(): boolean { return true; }
-    render(): string { return ""; }
+  setProps = (nextProps: BlockPropsType) => {
+    if (nextProps) {
+      const oldProps = { ...this.props };
+      this.props = { ...oldProps, ...nextProps };
+      this.eventBus.emit(Block.EVENTS.FLOW_CDU, oldProps, nextProps);
+    }
+  };
 
-    get element(): HTMLElement {
-        return this._element;
-    }
+  componentDidMount() {}
+  componentDidUpdate() {
+    return true;
+  }
 
-    getElement(): HTMLElement {
-        return this.element;
-    }
-  
-    show(): void {
-        this.getElement().style.display = "block";
-    }
-  
-    hide(): void {
-        this.getElement().style.display = "none";
-    }
+  render() {
+    return '';
+  }
+
+  get element(): HTMLElement {
+    return this._element;
+  }
+
+  getElement(): HTMLElement {
+    return this.element;
+  }
+
+  show(): void {
+    this.getElement().style.display = 'block';
+  }
+
+  hide(): void {
+    this.getElement().style.display = 'none';
+  }
 }
