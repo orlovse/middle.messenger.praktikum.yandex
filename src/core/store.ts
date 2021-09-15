49f28a3b858cbe63e60022f69e67cbbf13530d84
orlovse <hajoff@gmail.com>
2021-09-15 16:49:33 +0300

    change store

--- src/core/store.ts
+++ src/core/store.ts
@@ -1,25 +1,25 @@
-import { EventBus } from "../core/eventBus";
+import { EventBus } from '../core/eventBus';
 
 export enum STORE_EVENTS {
-    UPDATE = "update"
+  UPDATE = 'update'
 }
 
 export class Store<T> extends EventBus {
-    state: T | null;
+  state: T | null;
 
-    constructor(initialData: T | null = null) {
-        super();
-        this.state = initialData;
-    }
+  constructor(initialData: T | null = null) {
+    super();
+    this.state = initialData;
+  }
 
-    get() {
-        return this.state;
-    }
+  get() {
+    return this.state;
+  }
 
-    update(newData: T): void {
-        this.state = {...this.state, ...newData};
-        this.emit(STORE_EVENTS.UPDATE, newData);
-    }
+  update(newData: T) {
+    this.state = { ...this.state, ...newData };
+    this.emit(STORE_EVENTS.UPDATE, newData);
+  }
 }
 
 export const userStore = new Store();
