aad8a7ddff5314c7a40ffa220ae1bfcfef1f38fe
orlovse <hajoff@gmail.com>
2021-09-15 16:52:00 +0300

    add test basic routes

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,34 @@
-import 'regenerator-runtime/runtime'
-import "../static/styles.scss";
-import { render, Router } from "./core/router";
-import { Layout } from "./layout";
-import { Error, Login, Profile } from "./pages";
-import { Error500Page } from "./pages/test";
+import 'regenerator-runtime/runtime';
+import '../static/styles.scss';
+import { Router } from './core/router';
+import { Layout } from './layout';
+import { Error, Login, Profile } from './pages';
+import { appendBlockToDOM } from './utils/appendBlockToDOM';
 
 export enum ROUTES {
-  HOME = "/",
-  LOGIN = "/login",
-  SIGNIN = "/signin",
-  PROFILE_EDIT = "/profile-edit",
-  PASSWORD_EDIT = "/password-edit",
-  CHAT = "/chat/",
-  PROFILE = "/profile",
-  SERVER_ERROR = "/500",
-  NOT_FOUND = "/404",
+  HOME = '/',
+  LOGIN = '/login',
+  SIGNIN = '/signin',
+  PROFILE_EDIT = '/profile-edit',
+  PASSWORD_EDIT = '/password-edit',
+  CHAT = '/chat/',
+  PROFILE = '/profile',
+  SERVER_ERROR = '/500',
+  NOT_FOUND = '/404'
 }
 
-render('#app', Layout())
+appendBlockToDOM('#app', Layout());
 
-export const router = new Router("#layout-children");
+export const router = new Router('#layout-children');
 
 router
-    // .use(ROUTES.CHAT, ChatPage)
-    .use(ROUTES.LOGIN, Login())
-    // .use(ROUTES.SIGNIN, SigninPage)
-    // .use(ROUTES.PROFILE_EDIT, ProfileEditPage)
-    // .use(ROUTES.PASSWORD_EDIT, PasswordEditPage)
-    .use(ROUTES.PROFILE, Profile())
-    // .use(ROUTES.SERVER_ERROR, Error500Page)
-    .use(ROUTES.NOT_FOUND, Error({type: '404'}))
-    // .use(ROUTES.HOME, ChatsPage)
-    .use('/', Error500Page({errorCode: 'ababa', errorText: 'bababa'}))
-    .start();
+  // .use(ROUTES.CHAT, ChatPage)
+  .use(ROUTES.LOGIN, Login())
+  // .use(ROUTES.SIGNIN, SigninPage)
+  // .use(ROUTES.PROFILE_EDIT, ProfileEditPage)
+  // .use(ROUTES.PASSWORD_EDIT, PasswordEditPage)
+  .use(ROUTES.PROFILE, Profile())
+  // .use(ROUTES.SERVER_ERROR, Error500Page)
+  .use('/', Error({ type: '404' }))
+  // .use(ROUTES.HOME, ChatsPage)
+  .start();
