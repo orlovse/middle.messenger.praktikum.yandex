b3c88d2eafcedddf5bbdd1db7505a2b43f7da972
orlovse <hajoff@gmail.com>
2021-10-16 17:42:03 +0300

    add husky

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -2,4 +2,5 @@ export const TEMPLATE_REGEXP = /\{\{(.*?)\}\}/i;
 export const phoneRegExp =
   /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
 export const emailRegExp =
+  // eslint-disable-next-line max-len
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
